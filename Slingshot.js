ab6c6b70a696d179bd37e2b35b6ba3e8d6382df3
ushnish399 <nilanjanchakraborty5@gmail.com>
2019-12-31 20:49:53 +0530

    changed slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -7,8 +7,12 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        
     }
 
     fly(){
@@ -16,11 +20,29 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           push ();
+           
+            stroke(71, 33, 12);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3, pointA.x-30, pointA.y-10, 15, 30);
+
+            }
+            else {
+                strokeWeight(4);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3, pointA.x+25, pointA.y-10, 15, 30);
+            }
+          
+            pop ();
         }
     }
     
